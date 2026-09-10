4d0b7d42a18a6a650f2ad7e2178ca468ff4b5656
dhatchanamoorthia25-sketch <dhatchanamoorthia25@gmail.com>
2026-09-10 20:55:26 +0530

    Add python file

---
+++ binary tree . py.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+def height(root):
+    if root is None:
+        return -1
+
+    left_height = height(root.left)
+    right_height = height(root.right)
+
+    return 1 + max(left_height, right_height)
+
+
+# Create binary tree
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+# Find height
+print("Height of Binary Tree:", height(root))
