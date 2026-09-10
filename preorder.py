85c85ededfec96de6bf88af85ef06981aa0ef414
dhatchanamoorthia25-sketch <dhatchanamoorthia25@gmail.com>
2026-09-10 11:51:11 +0530

    Add files via upload

---
+++ preorder.py
@@ -0,0 +1,23 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+    def preorder(root):
+        if root:
+            print(root.data, end=" ")
+            preorder(root.left)
+            preorder(root.right)
+
+
+        root = Node(1)
+        root.left = Node(2)
+
+        root.right = Node(3)
+        root.left.left = Node(4)
+
+        root.left.right = Node(5)
+        print("Preorder traversal :")
+        preorder(root)
